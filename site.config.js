c97d3fe32794425254d2449c0a8fd4a580f8c5ee
childc <skimdcc@gmail.com>
2024-01-21 21:42:12 +0900

    Roll back CONFIG configuration

--- site.config.js
+++ site.config.js
@@ -24,12 +24,9 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2024, // If leave this empty, current year will be used.
-  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
-  seo: {
-    keywords: ["Swift", "SwiftUI", "iOS", "Combine"],
-  },
 
   // notion configuration (required)
   notionConfig: {
